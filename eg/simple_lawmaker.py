ee869c0a0d42688a63db2787929e4fe3d0d53763
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-08-30 19:45:17 -0400

    another small tweak

---
+++ eg/simple_lawmaker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from lawfaker import Legislator
+test = Legislator()
+
+print "%s %s %s %s" % (
+    test.title,
+    test.first_name,
+    "%s." % (test.middle_name[0]),
+    test.last_name
+)
+
+print "  %s memeber of the %s from %s, %s" % (
+    {
+        "R": "Republican",
+        "D": "Democrat",
+        "I": "Independent"
+    }[test.party],
+    {
+        "lower": "House",
+        "upper": "Senate"
+    }[test.chamber],
+    test.state.capital,
+    test.state,
+)
--- lawfaker/__init__.py
+++ lawfaker/__init__.py
@@ -1,6 +1,5 @@
-from lawfaker.models.legislator import Legislator
-
 __appname__ = "lawfaker"
 __version__ = "0.1"
 
-
+from lawfaker.models.legislator import Legislator
+from lawfaker.models.legislator import Person
